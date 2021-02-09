45eb41d6ebd52e4146b7c2b5d4a6e0f8e83c85b6
SMhd <smhd3000@gmail.com>
2021-02-09 20:39:42 +0330

    refactored dynamic array

--- heap/dynamic_array.c
+++ heap/dynamic_array.c
@@ -1,12 +1,13 @@
+#include "dynamic_array.h"
 #include <stdio.h>
 #include <malloc.h>
 
-typedef struct _dynamic_array
+struct dynamic_array
 {
     int *elements;
     int size;
     int capacity;
-} Array;
+};
 
 Array *newArray(int capacity)
 {
@@ -21,6 +22,17 @@ Array *newArray(int capacity)
 void doubleTheCapacity(Array *array)
 {
     array->elements = realloc(array->elements, array->capacity * 2 * sizeof(array->elements[0]));
+    array->capacity *= 2;
+}
+
+int indexIsValid(Array *array, int index)
+{
+    return index < array->size && index >= 0;
+}
+
+int isArrayEmpty(Array *array)
+{
+    return len(array) == 0;
 }
 
 int len(Array *array)
@@ -30,49 +42,117 @@ int len(Array *array)
 
 void set(Array *array, int index, int data)
 {
-    if (index < array->size && index >= 0)
-    {
+    if (indexIsValid(array, index))
         array->elements[index] = data;
-    }
 }
 
 int get(Array *array, int index)
 {
-    if (index < array->size && index >= 0)
-    {
+    if (indexIsValid(array, index))
         return array->elements[index];
+
+    return -1;
+}
+
+int swap(Array *array, int index1, int index2)
+{
+    if (indexIsValid(array, index1) && indexIsValid(array, index2))
+    {
+        int temp = get(array, index1);
+        set(array, index1, get(array, index2));
+        set(array, index2, temp);
+
+        return 1;
     }
 
     return -1;
 }
 
-void swap(Array *array, int index1, int index2)
-{
-    if (index1 < array->size && index2 < array->size)
-    {
-        int temp = get(array, index1);
-        set(array, index1, get(array, index2));
-        set(array, index2, temp);
-    }
-}
-
-void add(Array *array, int data)
+void addFirst(Array *array, int data)
 {
     if (array->size == array->capacity)
-    {
         doubleTheCapacity(array);
+
+    int length = len(array), previousItemData = 0;
+    array->size++;
+    for (int i = length; i > 0; i--)
+    {
+        previousItemData = get(array, i - 1);
+        set(array, i, previousItemData);
     }
+
+    set(array, 0, data);
+}
+
+void addLast(Array *array, int data)
+{
+    if (array->size == array->capacity)
+        doubleTheCapacity(array);
 
     array->elements[array->size] = data;
     array->size++;
 }
 
-void deleteLast(Array *array)
+int add(Array *array, int index, int data)
 {
-    if (len(array) > 0)
+    int length = len(array);
+    if (index > length && index < 0)
+        return -1;
+
+    if (length == array->capacity)
+        doubleTheCapacity(array);
+
+    int previousItemData = 0;
+    array->size++;
+    for (int i = length; i >= index + 1; i--)
     {
-        array->size--;
+        previousItemData = get(array, i - 1);
+        set(array, i, previousItemData);
     }
+
+    set(array, index, data);
+    return 1;
+}
+
+int deleteFirst(Array *array)
+{
+    if (isArrayEmpty(array))
+        return -1;
+
+    int size = len(array), nextItemData = 0;
+    for (int i = 0; i < size - 1; i++)
+    {
+        nextItemData = get(array, i + 1);
+        set(array, i, nextItemData);
+    }
+
+    array->size--;
+    return 1;
+}
+
+int deleteLast(Array *array)
+{
+    if (isArrayEmpty(array))
+        return -1;
+
+    array->size--;
+    return 1;
+}
+
+int delete (Array *array, int index)
+{
+    if (!indexIsValid(array, index) || isArrayEmpty(array))
+        return -1;
+
+    int length = len(array), nextItemData = 0;
+    for (int i = index; i < length; i++)
+    {
+        nextItemData = get(array, i + 1);
+        set(array, i, nextItemData);
+    }
+
+    array->size--;
+    return 1;
 }
 
 void deleteArray(Array *array)
@@ -81,3 +161,16 @@ void deleteArray(Array *array)
     free(array);
 }
 
+void printArray(Array *array)
+{
+    int length = len(array);
+    printf("(");
+
+    for (int i = 0; i < length - 1; i++)
+        printf("%i, ", array->elements[i]);
+
+    if (length > 0)
+        printf("%i)\n", array->elements[length - 1]);
+    else
+        printf(")\n");
+}
--- heap/dynamic_array.h
+++ heap/dynamic_array.h
@@ -1,20 +1,67 @@
 #ifndef DYNAMICARRAY_H
 #define DYNAMICARRAY_H
 
-typedef struct _dynamic_array
-{
-    int *elements;
-    int size;
-    int capacity;
-} Array;
+struct dynamic_array;
+typedef struct dynamic_array Array;
 
+// creates and returns a new empty dynamic array
 Array *newArray(int capacity);
-void set(Array *array, int index, int data);
-int get(Array *array, int index);
-void add(Array *array, int data);
-void deleteArray(Array *array);
+
+// checks whether dynamic array is empty
+int isArrayEmpty(Array *array);
+
+// returns length of the dynamic array
 int len(Array *array);
-void swap(Array *array, int index1, int index2);
-void deleteLast(Array *array);
+
+/*
+    swap items at index1 and index2
+    returns -1 if at least one of indexes are invalid
+*/
+int swap(Array *array, int index1, int index2);
+
+// updates value of array at position index to data
+void set(Array *array, int index, int data);
+
+/*
+    returns item at index position
+    returns -1 if index is invalid
+*/
+int get(Array *array, int index);
+
+// adds data to start of the dynamic array
+void addFirst(Array *array, int data);
+
+// adds data to end of the dynamic array
+void addLast(Array *array, int data);
+
+/*
+    adds data to specified index of the dynamic array
+    returns -1 if the index is invalid
+*/
+int add(Array *array, int index, int data);
+
+/*
+    deletes first item of the dynamic array
+    returns -1 if array is empty
+*/
+int deleteFirst(Array *array);
+
+/*
+    deletes last item of the dynamic array
+    returns -1 if array is empty
+*/
+int deleteLast(Array *array);
+
+/*
+    deletes item at position index of the dynamic array
+    returns -1 if array is empty or index is invalid
+*/
+int delete (Array *array, int index);
+
+// deletes the entire array and frees memory
+void deleteArray(Array *array);
+
+// prints array on the stdout
+void printArray(Array *array);
 
 #endif
